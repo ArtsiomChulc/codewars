462ae88fcf9bf4488f4d82c581cac482c158da5e
Artsiom <artemorsha@mail.ru>
2023-02-06 23:26:28 +0300

    reverse string

---
+++ index.js
@@ -0,0 +1,9 @@
+function solution(str) {
+	let splitStr = str.split('');
+	let reverseStr = splitStr.reverse();
+	let joinStr = reverseStr.join('');
+	return joinStr;
+}
+
+console.log(solution('world'));
+console.log(solution('word'));
