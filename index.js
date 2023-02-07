d8100e2a62bd518974a54ea93acb638becddb677
Artsiom <artemorsha@mail.ru>
2023-02-07 11:48:36 +0300

    convert boolean to str

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function doubleInteger(i) {
 	i *= 2;
 	return i;
 }
+
+//! Implement a function which convert the given boolean value into its string representation.
+function booleanToString(b) {
+	let string = String(b);
+	return string;
+	//your code here
+}
+
+console.log(booleanToString(true));
