c0440f38029550224705f9466d158db722c62ec8
Artsiom <artemorsha@mail.ru>
2023-02-07 12:26:24 +0300

    positive sum in array

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function booleanToString(b) {
 console.log(booleanToString(true));
 
 
+//! вычисление среднего значения массива 
 function betterThanAverage(classPoints, yourPoints) {
 	let average = classPoints.reduce((a, b) => a + b) / classPoints.length;
 	if (average > yourPoints) {
@@ -37,3 +38,23 @@ function betterThanAverage(classPoints, yourPoints) {
 }
 
 console.log(betterThanAverage([2, 2, 4, 2], 3));
+
+
+//! You get an array of numbers, return the sum of all of the positives ones.
+
+const arr = [];
+
+function positiveSum(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > 0) {
+			sum += arr[i];
+		}
+
+		if (arr.length === 0) {
+			sum = 0;
+		}
+	}
+	return sum;
+}
+console.log(positiveSum(arr));
